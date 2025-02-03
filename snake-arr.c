c002c3dc2497c1336e03a2d7645652dcd12e7a56
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-02-03 13:57:25 +0100

    Added collision on the boxes and disables it.

--- snake-arr.c
+++ snake-arr.c
@@ -5,7 +5,6 @@
 #include "raylib.h"
 #include "raymath.h"
 
-
 typedef struct{
     Vector2 position;
     Vector2 size;
@@ -13,13 +12,21 @@ typedef struct{
     float speed;
 }snake_t;
 
+typedef struct{
+    Vector2 position;
+    Vector2 size;
+    bool active;
+} eatable_t;
+
 int main (void){
     SetTargetFPS(60);
     InitWindow(800, 600, "SNAKE");
     
 
     snake_t snake = {{100, 100}, {20, 20}, {10, 0}, 100};
-    Rectangle rec = {snake.position.x, snake.position.y, snake.size.x, snake.size.y};
+    eatable_t box = {300, 300, 20, 20, true};
+
+
 
     while (!WindowShouldClose()) {
 
@@ -29,9 +36,26 @@ int main (void){
         ClearBackground(BLACK);
         DrawText("SNAKE", 50, 50, 20, WHITE);
 
-        
         snake.velocity = Vector2Scale(Vector2Normalize(snake.velocity), snake.speed);
         snake.position = Vector2Add(snake.position, Vector2Scale(snake.velocity, delta_time));
+
+
+        /*-------------------------------------------------------*/
+        /*----------------------Game-logic-----------------------*/
+        /*-------------------------------------------------------*/
+
+        Rectangle snake_rec = {snake.position.x, snake.position.y, snake.size.x, snake.size.y};
+
+        if(box.active == true){
+        Rectangle box_rec = {box.position.x, box.position.y, box.size.x, box.size.y};
+        
+        if(CheckCollisionRecs(snake_rec, box_rec)){
+            snake.size.y = snake.size.y+50;
+            box.active = false;
+        }
+
+        }
+
 
 
         /*-------------------------------------------------------*/
@@ -39,24 +63,28 @@ int main (void){
         /*-------------------------------------------------------*/
 
 
-        if(IsKeyDown(KEY_W) && snake.position.y)
+        if(IsKeyDown(KEY_W))
         {
-            snake.position.y = snake.position.y - snake.speed*delta_time;
+            snake.velocity.y = -10;
+            snake.velocity.x = 0;
         }
 
-        if(IsKeyDown(KEY_A) && snake.position.x)
+        if(IsKeyDown(KEY_A))
         {
-            snake.position.x = snake.position.x - snake.speed*delta_time;
+            snake.velocity.y = 0;
+            snake.velocity.x = -10;
         }
 
-        if(IsKeyDown(KEY_S) && snake.position.y)
+        if(IsKeyDown(KEY_S))
         {
-            snake.position.y = snake.position.y + snake.speed*delta_time;
+            snake.velocity.y = 10;
+            snake.velocity.x = 0;
         }
 
-        if(IsKeyDown(KEY_D) && snake.position.x)
+        if(IsKeyDown(KEY_D))
         {
-            snake.position.x = snake.position.x + snake.speed*delta_time;
+            snake.velocity.y = 0;
+            snake.velocity.x = 10;
         }
 
         /*-------------------------------------------------------*/
@@ -65,6 +93,9 @@ int main (void){
 
         DrawRectangle(snake.position.x, snake.position.y, snake.size.x, snake.size.y, GRAY);
 
+        if(box.active == true){
+        DrawRectangle(box.position.x, box.position.y, box.size.x, box.size.y, WHITE);
+        }
 
     EndDrawing();
     }   
