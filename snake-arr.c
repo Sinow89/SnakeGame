12ec048a7be0ba58e013b161ef820167e3f2e403
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-02-03 12:43:13 +0100

    Created snake struct and basic setup.

--- snake-arr.c
+++ snake-arr.c
@@ -6,15 +6,28 @@
 #include "raymath.h"
 
 
+typedef struct{
+    Vector2 position;
+    Vector2 size;
+    float speed;
+}snake_t;
+
 int main (void){
     SetTargetFPS(60);
     InitWindow(800, 600, "SNAKE");
+    float delta_time = GetFrameTime();
+
+    snake_t snake = {100, 100, 20, 20, 5};
 
     while (!WindowShouldClose()) {
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("SNAKE", 200, 350, 20, WHITE);
+        DrawText("SNAKE", 50, 50, 20, WHITE);
+
+        DrawRectangle(snake.position.x, snake.position.y, snake.size.x, snake.size.y, GRAY);
+
+        Rectangle rec = {snake.position.x, snake.position.y, snake.size.x, snake.size.y};
 
 
 
